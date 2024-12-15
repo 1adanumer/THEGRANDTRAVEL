16e456b06d38dc568df1572d5df1d7fd50ea9efb
Adan <codesofadan@gmail.com>
2024-12-15 20:15:19 +0500

    Fix casing of Login.css import in Login component

--- client/travel/src/assets/components/Login/Login.jsx
+++ client/travel/src/assets/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./login.css"; // Import the CSS file for styling
+import "./Login.css"; // Import the CSS file for styling
 
 class Login extends Component {
   constructor(props) {
